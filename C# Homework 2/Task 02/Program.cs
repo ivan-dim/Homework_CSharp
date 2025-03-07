0118d96c3c34365b47f3572793015f25d8de3285
ivan-dim <dimkovski.ivan@gmail.com>
2025-03-07 22:13:57 +0100

    Add files via upload

---
+++ C# Homework 2/Task 02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.Design;
+
+string[] studentsG1 = {"Zdravko", "Petko", "Stanko", "Branko", "Trajko"};
+string[] studentsG2 = {"Bobi", "Toni", "Dzoni", "Doni", "Ime5neznam"};
+
+Console.WriteLine("Choose student group (1 / 2)");
+int choice;
+int.TryParse(Console.ReadLine(), out choice);
+if (choice == 1)
+{
+    Console.WriteLine("Students of g1 are: ");
+    foreach (string student in studentsG1)
+    {
+        Console.WriteLine(student);
+    }
+}
+else if (choice == 2)
+{
+    Console.WriteLine("Students of g2 are: ");
+    {
+        foreach(string student in studentsG2)
+        {
+            Console.WriteLine(student);
+        }
+    }
+}
+else
+{
+    Console.WriteLine("invalid");
+}
---
+++ C# Homework 2/Task01/Program.cs
@@ -0,0 +1,18 @@
+﻿int[] numbers = new int[6];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Enter num" + (i+1) + ":");
+    int.TryParse(Console.ReadLine(), out numbers[i]);
+}
+
+int sumOfEven = 0;
+foreach(int number in numbers)
+{
+    if (number % 2 == 0)
+    {
+        sumOfEven += number;
+    }
+}
+
+Console.WriteLine("Res is " + sumOfEven);
