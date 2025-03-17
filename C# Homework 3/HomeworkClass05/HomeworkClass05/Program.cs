e568e7c2a8bfc9416bcff6e7a7124002ac2c0d11
Iccan <ivan-dim>
2025-03-17 13:22:42 +0100

    Add files via upload

---
+++ C# Homework 3/HomeworkClass05/HomeworkClass05/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Enter DoB(yr-mo-day");
+if (DateTime.TryParse(Console.ReadLine(), out DateTime birthDate))
+{
+    int age = AgeCalc(birthDate);
+    Console.WriteLine($"You're {age} yrs old");
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
+
+int AgeCalc(DateTime birthDate)
+{
+    DateTime today = DateTime.Today;
+    int age = today.Year - birthDate.Year;
+
+    if(birthDate.Date > today.AddYears(-age))
+    {
+        age--;
+    }
+    return age;
+}
